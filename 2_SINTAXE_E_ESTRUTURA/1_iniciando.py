d56296a081e724c39b1b8fae5946fec09d137078
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-18 09:30:21 -0300

    Add initial Python scripts for syntax and structure lessons

---
+++ 2_SINTAXE_E_ESTRUTURA/1_iniciando.py
@@ -0,0 +1,33 @@
+# Aula 1
+print("Meu texto")
+
+def saudacao():
+    print("Minha saudação")
+
+if __name__ == "__main__":
+    saudacao()    
+
+#* identação
+if 10 > 5:
+    print("10 é maior que 5")
+    print("Operação concluída com sucesso!")
+    print("Terminando o if")
+
+
+# blocos 
+if 70 > 18:
+    print("70 é maior que 18")
+    if 70 > 65:
+        print("Você é idoso")    
+
+# Aula 4 - Comentários
+
+#Aula 5 - Função print
+print("Exibir uma mensagem na tela")  
+
+print("Exibir, Mensagem ")
+
+print("Laranja", "Maçã", "Banana", sep="-")
+
+print("Carregando", end="... ")
+
---
+++ 2_SINTAXE_E_ESTRUTURA/2_avancando_sintaxe.py
@@ -0,0 +1,27 @@
+# Aula 6 - Recebendo dados com input
+nome_usuario = input("Qual é o seu nome? ")
+
+
+print(" o seu nome ", nome_usuario)
+
+# Aula 7 - Variáveis 
+temperatura = 32
+
+temperatura = temperatura + 12
+
+print( temperatura) 
+
+classe = "Magos"
+
+Characteres_class = "Guerreiro"
+
+#class, if, else, for, while são palavras reservadas e não podem ser usadas como nomes de variáveis
+
+# Aula 8 - Tipagem dinamica 
+
+numero = 10
+
+#int -> inteiro-> número inteiro
+print(type(numero))
+
+numero = "dez"
