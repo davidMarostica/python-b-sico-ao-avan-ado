077758204af055058d78a3b1331a9fef5b8ef35e
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-18 11:43:21 -0300

    operadores de funçoes

---
+++ 4_TIPOS-DE-DADOS/2_avancando.py
@@ -0,0 +1,24 @@
+# aula 1 nomeros complexos
+numero1 = 3 + 4 
+numero2 = 2 - 1 
+
+print(numero1)
+print(numero2)
+
+numero3 = complex(5, -3)
+print(numero3)
+
+print(numero1.real)
+print(numero1.imag)
+
+# aula 2 conversoes de tipos numericos
+numero_int = 10
+numero_float = float(numero_int)
+print(numero_float)
+
+
+numero_complexo = complex(numero_float)
+print(numero_complexo)
+
+print(10 + 10.4)
+print(10 + 10.44)  
