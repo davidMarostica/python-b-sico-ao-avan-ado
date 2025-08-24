d959a5bb9a3bd6ad9117ac1ab3e5a1d1932aa8be
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-24 10:15:28 -0300

    importação de módulo padrão

--- 25_MODULOS/1_iniciando.py
+++ 25_MODULOS/1_iniciando.py
@@ -7,4 +7,27 @@ import math
 
 print(math.sqrt(16))
 
+# aula 2 - modulos padrão / core modules
+print(math.pi)
+
+print(math.cos(math.radians(60)))
+
+import random
+
+print(random.randint(1, 10))
+
+lista = ["a", "b", "c", "d"]
+
+print(random.choice(lista))
+
+import datetime
+
+print(datetime.datetime.now())
+
+print(datetime.date.today())
+
+import os # operational system
+
+print(os.getenv("PATH"))
+
 
