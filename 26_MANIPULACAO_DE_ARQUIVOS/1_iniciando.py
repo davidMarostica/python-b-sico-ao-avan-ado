922a4034ee4603cfd5644211b2369205b97bf4f7
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-24 12:29:55 -0300

    formas de ler arquivos

--- 26_MANIPULACAO_DE_ARQUIVOS/1_iniciando.py
+++ 26_MANIPULACAO_DE_ARQUIVOS/1_iniciando.py
@@ -1,40 +1,73 @@
 # aula 1 - arquivos em python
-# w = write (escreve, sobrescreve o conteúdo)
-
+# w = write
 with open("exemplo.txt", "w") as arquivo:
-    arquivo.write("Olá arquivo Python")
+    arquivo.write("Ola arquivo Python")
 
-import csv  # Importação deve estar fora do bloco 'with'
+import csv
 
-with open("dados.csv", "w", newline='') as arquivo_csv:
+with open("dados.csv", "w") as arquivo_csv:
     writer = csv.writer(arquivo_csv)
     # csv = linhas e colunas, comma separated values
     writer.writerow(["Nome", "Idade"])
     writer.writerow(["Matheus", 33])
     writer.writerow(["Maria", 44])
-    writer.writerow(["David", 42])
 
 with open("imagem.jpg", "rb") as img:
     conteudo = img.read()
-    # print(conteudo)  # descomente se quiser ver os bytes da imagem
 
 # aula 2 - open e close
 arquivo = open("teste.txt", "w")
-print("Antes do close:", arquivo.closed)
+print(arquivo.closed)
 arquivo.write("Escrevi e pronto...")
-print("Durante o uso:", arquivo.closed)
+print(arquivo.closed)
 arquivo.close()
-print("Depois do close:", arquivo.closed)
+print(arquivo.closed)
+
+# open => abre o arquivo
+# close => fecha o arquivo
+
+# a gente realize operacoes com o with
+
+# se nao utilizar ou se pegar codigo legado/antigo, checar se há o close
 
 # aula 3 - modos de arquivos
 with open("exemplo.txt", "r") as arquivo:
-    print("Conteúdo atual:", arquivo.read())
+    print(arquivo.read())
+
 
 with open("exemplo.txt", "w") as arquivo:
     arquivo.write("Sobrescrevendo tudo!")
 
 with open("exemplo.txt", "a") as arquivo:
-    arquivo.write("\nIsso está depois do que já foi escrito...")
+    arquivo.write("Isso está dps do que já foi escrito...")
+
+with open("exemplo.txt", "a") as arquivo:
+    arquivo.write("Isso está dps do que já foi escrito... 2")
 
 with open("exemplo.txt", "w") as arquivo:
     arquivo.write("Sobrescrevendo tudo!")
+
+# aula 4 - leitura
+with open("exemplo2.txt", "r") as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo) 
+
+with open("exemplo2.txt", "r") as arquivo:
+    linha1 = arquivo.readline()
+    linha2 = arquivo.readline()
+    linha3 = arquivo.readline()
+
+    print("Linha 1:" + linha1)
+    print("Linha 2:" + linha2)
+    print("Linha 3:" + linha3)
+
+with open("exemplo2.txt", "r") as arquivo:
+    linhas = arquivo.readlines()
+
+    print(linhas)
+
+with open("exemplo2.txt", "r") as arquivo:
+    linhas = arquivo.readlines()
+
+    for linha in linhas:
+        print(linha)
