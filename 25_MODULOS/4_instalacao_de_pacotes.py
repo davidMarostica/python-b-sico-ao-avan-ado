a9c767afea227514b59208c51ed29ee2f7194d22
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-24 11:28:05 -0300

    pypi e o modo rich

--- 25_MODULOS/4_instalacao_de_pacotes.py
+++ 25_MODULOS/4_instalacao_de_pacotes.py
@@ -5,3 +5,12 @@ response = requests.get("https://api.github.com")
 
 print("Status da req. ", response.status_code)
 print("Cabeçalhos: ", response.headers)
+
+# 2 - pypi
+from rich.console import Console
+
+console = Console()
+
+console.print("[bold magenta]Olá Mundo[/bold magenta]")
+
+console.print("[green]Testando[/green] [underline]Testando underline[/underline]")
