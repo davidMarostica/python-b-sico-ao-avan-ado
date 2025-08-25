64fe9edec35015f4d828a80360e0b880c737acfa
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-25 13:01:05 -0300

    atributos

--- 29_INTRO_POO/1_iniciando.py
+++ 29_INTRO_POO/1_iniciando.py
@@ -42,3 +42,34 @@ carro1.ano = 2000
 print(carro1.ano)
 
 carro1.detalhes()
+
+# aula 3 - atributos (propriedades)
+
+class Usuario:
+    def __init__(self, nome, email):
+        self.nome = nome
+        self.email = email
+
+
+usuario = Usuario("João", "joao@email.com")
+usuario2 = Usuario("Pedro", "pedro@email.com")
+
+print(usuario.nome)
+print(usuario.email)
+print(usuario2.email)
+
+usuario.nome = "João Silva"
+
+print(usuario.nome)
+
+class Retangulo:
+    def __init__(self, altura, largura):
+        self.largura = largura
+        self.altura = altura
+
+    def calcular_area(self):
+        return self.largura * self.altura
+    
+ret = Retangulo(10, 2)
+
+print(f"O valor da area do retangulo é {ret.calcular_area()}")
