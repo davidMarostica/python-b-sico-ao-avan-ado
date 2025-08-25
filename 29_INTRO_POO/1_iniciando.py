08befb78e6000a45a138efd0bcd3434a40ebe381
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-25 12:55:46 -0300

    propriedade e metodos

--- 29_INTRO_POO/1_iniciando.py
+++ 29_INTRO_POO/1_iniciando.py
@@ -11,3 +11,34 @@ class Animal:
 animal = Animal("Cachorro")
 
 print(f"Espécie do animal: {animal.especie}")
+
+# aula 2 - instancia
+
+class Carro:
+    # propriedade
+    def __init__(self, modelo, ano):
+        self.modelo = modelo
+        self.ano = ano
+
+    #função ou metodos 
+    def detalhes(self):
+        print(f"O carro é do modelo {self.modelo} e foi fabricado em {self.ano}")
+
+carro1 = Carro("Fusca", 1976)
+
+carro2 = Carro("Civic", 2010)
+
+print(carro1.modelo)
+
+print(carro2.modelo)
+
+carro1.detalhes()
+
+# prop -> caracteristicas do obj, o nome obj.prop
+# metodos -> acoes, o nome obj.metodo()
+
+carro1.ano = 2000
+
+print(carro1.ano)
+
+carro1.detalhes()
