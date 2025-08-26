8feaabb5682619fa713ad878e4b9c9a7e1217b57
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-26 10:39:27 -0300

    manipulacao de atributos com @property

--- 31_AVANCANDO_POO/1_intro.py
+++ 31_AVANCANDO_POO/1_intro.py
@@ -38,3 +38,50 @@ conta = ContaBancaria("Pedro", 1000)
 # print(conta.__nome) = erro
 
 conta.exibir_dados()
+
+
+# aula 2 - manipulacao de atributos com @property
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.__nome = nome
+        self.__preco = preco
+
+    def get_preco(self):
+        return f"R${self.__preco}"
+    
+    def set_preco(self, preco):
+        if preco < 0:
+            print("O preço não pode ser negativo!")
+            return
+        self.__preco = preco
+
+produto = Produto("Notebook", 4000)
+
+print(f"Preço atual do produto: {produto.get_preco()}")
+
+produto.set_preco(1000)
+
+print(f"Preço atual do produto: {produto.get_preco()}")
+
+# @property
+class Carro:
+    def __init__(self, modelo):
+        self.__modelo = modelo
+
+    @property
+    def modelo(self):
+        return self.__modelo
+    
+    @modelo.setter
+    def modelo(self, novo_modelo):
+        self.__modelo = novo_modelo
+    
+
+carro = Carro("Civic")
+
+print(carro.modelo)
+
+carro.modelo = "Civic 2010"
+
+print(carro.modelo)
