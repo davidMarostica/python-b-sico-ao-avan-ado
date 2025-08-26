135e5900842ee5f4e5a58a231223f9cf3c3f58da
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-26 12:30:52 -0300

    decorators simples

--- 32_DECORATORS/1_iniciando.py
+++ 32_DECORATORS/1_iniciando.py
@@ -46,3 +46,41 @@ multiplicador = criar_multiplicador(3)
 print(multiplicador(10))
 print(multiplicador(15))
 print(multiplicador(20))
+
+
+# aula 3 - decorators simples
+
+def log_decorador(func):
+    def wrapper(*args, **kwargs):
+        print(f"Chamando a funçao: {func.__name__}")
+        resultado = func(*args, **kwargs)
+        print(f"Função: {func.__name__} executada com sucesso.")
+        return resultado
+    return wrapper
+
+@log_decorador
+def soma(a, b):
+    return a + b
+
+@log_decorador
+def multiplicacao(a, b, c):
+    return a * b * c
+
+
+print(soma(4, 5))
+print(multiplicacao(4, 5, 6))
+
+
+def validar_positivo(func):
+    def wrapper(x):
+        if x < 0:
+            raise ValueError("O número deve ser positivo.")
+        return func(x)
+    return wrapper
+
+@validar_positivo
+def calcular_raiz_quadrada(x):
+    return x ** .5
+
+print(calcular_raiz_quadrada(10))
+# print(calcular_raiz_quadrada(-10)) -> dispara o erro
