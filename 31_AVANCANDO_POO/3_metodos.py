3057d9ce32f9a4259851d4efcc13551de01239d2
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-26 11:53:10 -0300

    metodo de class

--- 31_AVANCANDO_POO/3_metodos.py
+++ 31_AVANCANDO_POO/3_metodos.py
@@ -26,3 +26,27 @@ print(Matematica.soma(10, 2))
 print(Matematica.calcular_area_retangulo(12, 5))
 
 # print(Matematica.soma2(10, 2)) -> erro, precisa de objeto
+
+# aula 2 - class methods
+
+class ContaBancaria:
+    total_contas = 0
+
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+
+        ContaBancaria.total_contas += 1
+
+    @classmethod
+    def obter_total_de_contas(cls):
+        return cls.total_contas
+    
+
+print(f"Nós temos {ContaBancaria.obter_total_de_contas()} contas no banco!")
+
+c1 = ContaBancaria("Matheus", 1000)
+c2 = ContaBancaria("Pedro", 2000)
+c3 = ContaBancaria("João", 3000)
+
+print(f"Nós temos {ContaBancaria.obter_total_de_contas()} contas no banco!")
