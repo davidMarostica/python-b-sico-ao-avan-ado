c9d94b316494f4c06bf27e89d04017c6a0ee3e0a
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-29 09:50:46 -0300

    datetime

--- 34_DATA_E_HORA/1_iniciando.py
+++ 34_DATA_E_HORA/1_iniciando.py
@@ -9,3 +9,22 @@ data_inicio = datetime.now()
 data_consulta = data_inicio + timedelta(days=7)
 
 print(data_consulta)
+
+# aula 2 - datetime
+
+data = date(2024, 5, 12)
+
+print(data)
+
+tempo = time(13, 33, 12)
+
+print(tempo)
+
+data_com_horario = datetime(2020, 10, 20, 14, 30, 10)
+data_com_horario_2 = datetime(2021, 12, 24, 14, 30, 10)
+
+print(data_com_horario)
+
+diferenca = data_com_horario_2 - data_com_horario
+
+print(diferenca)
