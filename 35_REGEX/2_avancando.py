d017d2aa03c9dbd59275c275e769182778e66654
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-29 16:34:47 -0300

    conjuntos

---
+++ 35_REGEX/2_avancando.py
@@ -0,0 +1,18 @@
+# aula 1 -  conjuntos
+import re
+
+texto = "Hoje é dia 20 de Janeiro de 2024"
+
+print(re.findall(r"\d+", texto))
+
+texto = "Python_3 é incrível!"
+
+print(re.findall(r"\w+", texto))
+
+texto = "Texto com espaços e \n quebras de \n linha"
+
+print(re.findall(r"\s", texto))
+
+texto = "abc 123 xyz"
+
+print(re.findall(r"[a-c1-3]+", texto))
