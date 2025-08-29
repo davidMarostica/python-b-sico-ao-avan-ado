d7d37636f69c7634a01514c853ec580353e52d5b
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-29 15:11:51 -0300

    calculo com datas

--- 34_DATA_E_HORA/2_modulos.py
+++ 34_DATA_E_HORA/2_modulos.py
@@ -13,3 +13,24 @@ try:
     data_invalida = datetime.strptime("200/10/24", "%d/%m/%Y")
 except ValueError as e:
     print(f"Erro ao converter data: {e}")
+
+    # aula 2- calculo com datas
+
+data1 = datetime(2024, 12, 25)
+data2 = datetime(2025, 9, 12)
+
+diferenca = data2 - data1
+
+print(diferenca.days)
+
+nova_data = data1 + timedelta(days=5)
+nova_data_semanas = data1 - timedelta(weeks=3)
+
+print(nova_data)
+print(nova_data_semanas)
+
+periodo = timedelta(days=3, hours=3)
+
+data_evento = data1 + periodo
+
+print(data_evento)
