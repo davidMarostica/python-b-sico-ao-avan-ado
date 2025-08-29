2d3180d577a0a177f2f0cf1a3323cca6a73710a0
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-29 16:21:45 -0300

    como funciona a regex

--- 35_REGEX/1_iniciando.py
+++ 35_REGEX/1_iniciando.py
@@ -12,3 +12,19 @@ frase = "Python é muito legal!"
 nova_frase = re.sub(r"legal", "massa", frase)
 
 print(nova_frase)
+
+# aula 2 - como funciona a regex
+
+texto = "123 abc 456 abc 789 abc def jip kew"
+
+print(re.findall(r"abc", texto))
+
+print(re.findall(r"[a-e]+", texto))
+
+# Python -> python => nao encontra
+
+# PyThoN
+
+texto = "Python é muito bom, python é muito legal, PyThOn é demais!"
+
+print(re.findall(r"python", texto, re.IGNORECASE))
