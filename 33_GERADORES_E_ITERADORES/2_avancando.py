ad05fbba8f68ec842f791081a51fb360e9c67907
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-29 09:02:24 -0300

    func. geradoras x expressoes geradoras

--- 33_GERADORES_E_ITERADORES/2_avancando.py
+++ 33_GERADORES_E_ITERADORES/2_avancando.py
@@ -13,3 +13,19 @@ import sys
 
 print(f"Tamanho em bytes gerador: {sys.getsizeof(lista)} ")
 print(f"Tamanho em bytes gerador: {sys.getsizeof(gerador)} ")
+
+# aula 2 - func. geradoras x expressoes geradoras
+
+# num pares => x % 2 == 0
+pares = (x for x in range(10) if x % 2 == 0)
+
+print(list(pares))
+
+# fibonacci -> n consigo em uma linha = funcoes
+def numero_fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a,b = b, a + b
+
+print(list(numero_fibonacci(15)))
