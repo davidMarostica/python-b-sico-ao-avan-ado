c9e2d174d74246c6e05f4a5edef66ec2d94200cb
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-08-29 15:30:14 -0300

    pytz

--- 34_DATA_E_HORA/3_avancando.py
+++ 34_DATA_E_HORA/3_avancando.py
@@ -24,3 +24,21 @@ print(calendario_mes_2)
 for ano in range(2010, 2040):
     if calendar.isleap(ano):
         print(f"{ano} é bissexto")
+
+
+        # aula 2 - pytz
+import pytz
+from datetime import datetime
+
+zona_ny = pytz.timezone("America/New_York")
+data_ny = datetime.now(zona_ny)
+
+print(data_ny)
+
+print(pytz.all_timezones[:100])
+
+zona_br = pytz.timezone("America/Sao_Paulo")
+
+data_br = datetime(2024, 2, 1, tzinfo=zona_br)
+
+print(data_br)
